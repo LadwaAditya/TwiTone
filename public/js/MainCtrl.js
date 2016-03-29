1b5a4bddc91406decc65338d8f6a759b6b4160e9
Aditya Ladwa <ladwa.aditya@gmail.com>
2016-03-29 21:45:58 +0530

    Added analysis client side code

--- public/js/MainCtrl.js
+++ public/js/MainCtrl.js
@@ -4,13 +4,18 @@ app.controller('MainCtrl', ['$scope', '$http', function ($scope, $http) {
 
 
     $scope.getTweets = function () {
-        console.log($scope.user.name);
         $http.get('tweet/' + $scope.user.name).success(function (data) {
             var tweets = [];
             $.each(data, function (i, obj) {
                 tweets.push(obj.text);
             });
             $scope.tweets = tweets;
+        })
+    };
+
+    $scope.analyze = function (tweet) {
+        $http.get('tone/' + tweet).success(function (data) {
+            console.log(data);
         })
     }
 }]);
