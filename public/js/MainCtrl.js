17dff89ac5ab3165366e6e7c2ad78275e002e96c
Aditya Ladwa <ladwa.aditya@gmail.com>
2016-03-30 00:13:25 +0530

    fect : tweet tone values are parsed and added to an array

--- public/js/MainCtrl.js
+++ public/js/MainCtrl.js
@@ -1,5 +1,15 @@
 var app = angular.module('TwiTone', []);
 
+
+function range(value, min, max) {
+    return (value - min) / (max - min);
+}
+
+
+function rangeToPercent(value, min, max) {
+    return Math.round(100 * range(value, min, max));
+}
+
 app.controller('MainCtrl', ['$scope', '$http', function ($scope, $http) {
 
 
@@ -15,7 +25,35 @@ app.controller('MainCtrl', ['$scope', '$http', function ($scope, $http) {
 
     $scope.analyze = function (tweet) {
         $http.get('tone/' + tweet).success(function (data) {
-            console.log(data);
+            var obj = data["document_tone"]["tone_categories"];
+
+            var emotionalTone = obj[0]["tones"];
+            var writeingTone = obj[1]["tones"];
+            var socialTone = obj[2]["tones"];
+
+            var emotionalScore = [];
+            var writeingScore = [];
+            var socialScore = [];
+
+            var emotionalName = [];
+            var writeingName = [];
+            var socialName = [];
+
+            $.each(emotionalTone, function (i, o) {
+                emotionalScore.push(o["score"]);
+                emotionalName.push(o["tone_name"]);
+            });
+
+            $.each(writeingTone, function (i, o) {
+                writeingScore.push(o["score"]);
+                writeingName.push(o["tone_name"]);
+            });
+
+            $.each(socialTone, function (i, o) {
+                socialScore.push(o["score"]);
+                socialName.push(o["tone_name"]);
+            });
+                       
         })
     }
 }]);
